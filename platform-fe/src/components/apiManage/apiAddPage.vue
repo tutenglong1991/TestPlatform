1fff0a825489c4ce45f789b2f26adb8643a82774
hemeilong <hemeilong@tuzhanai.com>
2019-12-26 23:33:00 +0800

    修改创建接口前端页面结构和必填字段校验

--- platform-fe/src/components/apiManage/apiAddPage.vue
+++ platform-fe/src/components/apiManage/apiAddPage.vue
@@ -8,61 +8,75 @@
       </el-breadcrumb>
     </el-header>
     <el-main>
-      <div style="width:100%;">
-        <el-form :model="apidata" ref="submitForm1" :rules="rules" label-width="80px" class="demo-dynamic" style="display: flex; justify-content: left;">
-          <el-form-item label="接口名称" prop="apiName">
-            <el-input v-model="apidata.apiName" autocomplete="off" placeholder="请输入接口名称"></el-input>
-          </el-form-item>
-          <el-form-item label="接口地址" prop="apiPath">
-            <el-input v-model="apidata.apiPath" autocomplete="off" placeholder="请输入接口地址"></el-input>
-          </el-form-item>
-          <el-form-item label="域名/ip" prop="apiDomain">
-            <el-input v-model="apidata.apiDomain" autocomplete="off" placeholder="请选择域名/ip"></el-input>
-          </el-form-item>
-          <el-form-item label="网络协议" prop="netProtocol">
-            <el-select v-model="apidata.netProtocol" clearable autocomplete="off" placeholder="请选择网络协议">
-              <el-option
-                v-for="h in apiDataSelection.netProtocol"
-                :key="h.value"
-                :label="h.label"
-                :value="h.value">
-              </el-option>
-            </el-select>
-          </el-form-item>
-        </el-form>
-      </div>
-      <div style="width:100%; border-bottom: 1px solid #bbbcbf59;">
-        <el-form :model="apidata" ref="submitForm2" :rules="rules" label-width="80px" class="demo-dynamic" style="display: flex; justify-content: left;">
-          <el-form-item label="请求方式" prop="reqMethods">
-            <el-select v-model="apidata.reqMethods" clearable autocomplete="off" placeholder="请选择网络协议">
-              <el-option
-                v-for="m in apiDataSelection.reqMethods"
-                :key="m.value"
-                :label="m.label"
-                :value="m.value">
-              </el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="设置UA" prop="reqUa">
-            <el-input v-model="apidata.reqUa" autocomplete="off" placeholder="请设置接口请求UA"></el-input>
-          </el-form-item>
-          <el-form-item label="所属项目" prop="ownPro">
-            <el-select v-model="apidata.ownPro" clearable autocomplete="off" placeholder="请选择所属项目">
-              <el-option
-                v-for="item in apiDataSelection.projectOptions"
-                :key="item.id"
-                :label="item.projectName"
-                :value="item.id">
-                <span style="float: left">{{ item.projectName }}</span>
-                <span style="float: right; color: #8492a6; font-size: 13px">{{ item.id }}</span>
-              </el-option>
-            </el-select>
-          </el-form-item>
-          <el-form-item label="所属模块" prop="ownGroup">
-            <el-input v-model="apidata.ownGroup" autocomplete="off" placeholder="请选择接口分组"></el-input>
-          </el-form-item>
-        </el-form>
-      </div>
+      <el-form :model="apidata" ref="submitForm1" :rules="rules" label-width="80px" class="demo-dynamic" style="width:85%">
+        <el-row>
+          <el-col :span='6'>
+            <el-form-item label="接口名称" prop="apiName">
+              <el-input v-model="apidata.apiName" autocomplete="off" placeholder="请输入接口名称"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span='6'>
+            <el-form-item label="接口地址" prop="apiPath">
+             <el-input v-model="apidata.apiPath" autocomplete="off" placeholder="请输入接口地址"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span='6'>
+            <el-form-item label="域名/ip" prop="apiDomain">
+              <el-input v-model="apidata.apiDomain" autocomplete="off" placeholder="请选择域名/ip"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span='6'>
+            <el-form-item label="网络协议" prop="netProtocol">
+              <el-select v-model="apidata.netProtocol" clearable autocomplete="off" placeholder="请选择网络协议">
+                <el-option
+                  v-for="h in apiDataSelection.netProtocol"
+                  :key="h.value"
+                  :label="h.label"
+                  :value="h.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span='6'>
+            <el-form-item label="请求方式" prop="reqMethods">
+              <el-select v-model="apidata.reqMethods" clearable autocomplete="off" placeholder="请选择网络协议">
+                <el-option
+                  v-for="m in apiDataSelection.reqMethods"
+                  :key="m.value"
+                  :label="m.label"
+                  :value="m.value">
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span='6'>
+            <el-form-item label="设置UA" prop="reqUa">
+              <el-input v-model="apidata.reqUa" autocomplete="off" placeholder="请设置接口请求UA"></el-input>
+            </el-form-item>
+          </el-col>
+          <el-col :span='6'>
+            <el-form-item label="所属项目" prop="ownPro">
+              <el-select v-model="apidata.ownPro" clearable autocomplete="off" placeholder="请选择所属项目">
+                <el-option
+                  v-for="item in apiDataSelection.projectOptions"
+                  :key="item.id"
+                  :label="item.projectName"
+                  :value="item.id">
+                  <span style="float: left">{{ item.projectName }}</span>
+                  <span style="float: right; color: #8492a6; font-size: 13px">{{ item.id }}</span>
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :span='6'>
+            <el-form-item label="所属模块" prop="ownGroup">
+              <el-input v-model="apidata.ownGroup" autocomplete="off" placeholder="请选择接口分组"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
       <el-row style="margin-left: 12px">
         <el-button @click="addParam" type="primary" size="small">格式化请求入参</el-button>
         <el-button @click="addParam" type="primary" size="small">运行接口</el-button>
@@ -72,7 +86,7 @@
       </el-input>
       <el-input class='create-text' type="textarea" :rows="10" placeholder="返回参数..." v-model="respTextarea" style="margin-left:12px; width:45%;">
       </el-input>
-      <el-form :model="apidata" ref="apidata" label-width="100px" class="demo-dynamic">
+      <el-form :model="apidata" ref="submitForm2" :rules='rules' label-width="0px" class="demo-dynamic">
         <el-row :gutter="20" style="font-family: 'Avenir', Helvetica, Arial, sans-serif; font-size: 14px; color: #606266">
           <el-col :span="4" :push="1"><span>参数名称</span></el-col>
           <el-col :span="3"><span>参数值</span></el-col>
@@ -82,19 +96,29 @@
         </el-row>
         <el-row :gutter="20" v-for="(kv, index) in apidata.parameters" :key="index" style="margin-left: 2px;">
           <el-col :span="3">
-            <el-input v-model="kv.paramName" placeholder="请输入参数名" size="small" clearable></el-input>
+            <el-form-item prop="paramName">
+              <el-input v-model="kv.paramName" placeholder="请输入参数名" size="small" clearable></el-input>
+            </el-form-item>
           </el-col>
           <el-col :span="4">
-            <el-input v-model="kv.paramValue" placeholder="请输入参数值" size="small" clearable></el-input>
+            <el-form-item prop="paramValue">
+              <el-input v-model="kv.paramValue" placeholder="请输入参数值" size="small" clearable></el-input>
+            </el-form-item>
           </el-col>
           <el-col :span="3">
-            <el-input v-model="kv.isForce" placeholder="请选择是否必选" size="small" clearable></el-input>
+            <el-form-item prop="isForce">
+              <el-input v-model="kv.isForce" placeholder="请选择是否必选" size="small" clearable></el-input>
+            </el-form-item>
           </el-col>
           <el-col :span="3">
-            <el-input v-model="kv.paramType" placeholder="请选择参数类型" size="small" clearable></el-input>
+            <el-form-item prop="paramType">
+              <el-input v-model="kv.paramType" placeholder="请选择参数类型" size="small" clearable></el-input>
+            </el-form-item>
           </el-col>
-          <el-col :span="5">
-            <el-input v-model="kv.paramRemark" placeholder="请输入参数备注信息" size="small" clearable></el-input>
+          <el-col :span="5" prop="paramRemark">
+            <el-form-item>
+              <el-input v-model="kv.paramRemark" placeholder="请输入参数备注信息" size="small" clearable></el-input>
+            </el-form-item>
           </el-col>
           <el-col :span="6">
             <el-button @click="addParam" type="primary" icon="el-icon-plus" size="small">新增参数</el-button>
@@ -153,14 +177,20 @@ export default {
         netProtocol: [{ required: true, message: '网络协议不能为空', trigger: 'change' }],
         reqMethods: [{ required: true, message: '请求方式不能为空', trigger: 'change' }],
         ownGroup: [{ required: true, message: '所属模块不能为空', trigger: 'blur' }],
-        ownPro: [{ required: true, message: '所属项目不能为空', trigger: 'change' }]
+        ownPro: [{ required: true, message: '所属项目不能为空', trigger: 'change' }],
+        paramName: [{ required: true, message: '参数名不能为空', trigger: 'blur' }],
+        paramValue: [{ required: false }],
+        isForce: [{ required: true, message: '参数是否必选不能为空', trigger: 'blur' }],
+        paramType: [{ required: true, message: '参数类型不能为空', trigger: 'blur' }],
+        paramRemark: [{ required: false }]
       }
     }
   },
   methods: {
-    addApi (form1Name, form2Name) {
-      this.$refs[form1Name].validate((valid) => {
-        if (valid) {
+    addApi (formName1, formName2) { // 第一个参数是接口必要信息表单，第二个参数是接口对应参数的表单
+      let isParamsCheckedPass = this.validParams(formName2)
+      this.$refs[formName1].validate((valid) => {
+        if (valid && isParamsCheckedPass) {
           let param = JSON.stringify(this.apidata)
           console.log(param)
           return this.$axios.post('/apiAutoTest/apiInfo/addApi', param).then(response => {
@@ -183,6 +213,16 @@ export default {
             console.log('发送Ajax请求,' +
             '请求失败', response)
           })
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    validParams (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          return true
         } else {
           console.log('error submit!!')
           return false
@@ -270,9 +310,6 @@ export default {
     color: #FFF;
     background-color: #04aa51;
     border-color: #04aa51;
-  }
-  .el-input--suffix .el-input__inner {
-    padding-right: 15px;
   }
   .el-select .el-input__inner:hover {
     border-color: #04aa51;
